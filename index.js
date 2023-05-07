af511be12dd7f8d9760b0b4dff7f23394b9ee152
GreatDeveloper66 <ashaffe@gmail.com>
2023-05-07 08:27:57 -0500

    Update app with jquery installation

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 4000
+const { JSDOM } = require( "jsdom" );
+const { window } = new JSDOM( "" );
+const $ = require( "jquery" )( window );
 
 app.use(express.static(__dirname + '/public'))
 
