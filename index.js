4ac70a00dff4696e077e1d5060483c3331a8f353
GreatDeveloper66 <ashaffe@gmail.com>
2023-05-07 09:41:29 -0500

    Wasted time debugging

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const app = express();
 const port = 4000
-const { JSDOM } = require( "jsdom" );
-const { window } = new JSDOM( "" );
-const $ = require( "jquery" )( window );
-
 app.use(express.static(__dirname + '/public'))
 
 app.listen(port ,() => {
